3a1c2d917520ef5dc4b2bc412f13b3dadc571d08
Ethan Martinez <30417455+EthanM27@users.noreply.github.com>
2022-12-24 20:19:06 -0500

    added mayhem support

---
+++ mayhem/fuzz_spellcheck.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+import atheris
+import sys
+
+with atheris.instrument_imports():
+    from spellchecker import SpellChecker
+
+
+@atheris.instrument_func
+def test_input(data):
+    spell = SpellChecker()
+    
+    fdp = atheris.FuzzedDataProvider(data)
+    s = fdp.ConsumeString(4096)
+
+    spell.correction(s)
+    spell.candidates(s)
+
+def main():
+    atheris.Setup(sys.argv, test_input)
+    atheris.Fuzz()
+
+if __name__ == "__main__":
+    main()
